1f844655baba9e0c8bb1f0d6b37212d588e29440
kumilachew-g <kumilachew1212@gmail.com>
2022-09-12 23:58:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+/*eslint-disable */
+import ReactDOM from 'react-dom/client';
 import React from 'react';
-import ReactDOM from 'react-dom';
-//component file
-import TodoContainer from './components/TodoContainer';
-ReactDOM.render(<TodoContainer />, document.getElementById('root'));
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TodoContainer from './functionBased/components/TodoContainer';
+import About from './functionBased/pages/About';
+import NotMatch from './functionBased/pages/NotMatch';
+import Navbar from './functionBased/components/Navbar';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<TodoContainer />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
